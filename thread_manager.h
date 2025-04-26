2647ca843efea5ffcd1445686d0eef5f316552c2
Arsalan Mir <119820647+MIR39X@users.noreply.github.com>
2025-04-27 01:10:52 +0500

    Update thread_manager.h

--- thread_manager.h
+++ thread_manager.h
@@ -1,11 +1,11 @@
-// thread_manager.h
+
 #ifndef THREAD_MANAGER_H
 #define THREAD_MANAGER_H
 
 #include <pthread.h>
 #include <semaphore.h>
 
-// Declare global variables (do not define them here)
+// Declare global variables
 extern pthread_t readers[5];
 extern pthread_t writers[2];
 extern sem_t rw_mutex;
@@ -16,8 +16,8 @@ extern pthread_mutex_t mutex;
 void* reader_thread(void* arg);
 void* writer_thread(void* arg);
 
-// Declare functions defined in book_operations.c
-void viewBookCatalog();  // Declare the function to view the book catalog
-void addBook();  // Declare the function to add a book
-
+void viewBookCatalog(); 
+void addBook();  
+void removeBook();
+void borrowBook();
 #endif
